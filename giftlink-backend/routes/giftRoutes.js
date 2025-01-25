34f365676b6dc3538197d0a8a72d94369c93ea47
yuxinens <zendhop@gmail.com>
2025-01-24 23:45:41 -0500

    make an api service

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,16 +1,16 @@
 router.get('/', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        // Task 1: Connect to MongoDB
+        const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        // Task 2: Retrieve the gift collection
+        const collection = db.collection('gifts');
 
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        // Task 3: Fetch all gifts
+        const gifts = await collection.find({}).toArray();
 
-        // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        // Task 4: Return the gifts
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
@@ -19,16 +19,15 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        // Task 1: Connect to MongoDB
+        const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        // Task 2: Retrieve the gift collection
+        const collection = db.collection('gifts');
 
+        // Task 3: Find a specific gift by ID
         const id = req.params.id;
-
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        const gift = await collection.findOne({ id: id });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -40,20 +39,3 @@ router.get('/:id', async (req, res) => {
         res.status(500).send('Error fetching gift');
     }
 });
-
-
-
-// Add a new gift
-router.post('/', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection("gifts");
-        const gift = await collection.insertOne(req.body);
-
-        res.status(201).json(gift.ops[0]);
-    } catch (e) {
-        next(e);
-    }
-});
-
-module.exports = router;
